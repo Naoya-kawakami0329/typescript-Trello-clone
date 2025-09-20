cf7697a3bfd6e3fe23363c12ac2b2a530944ae45
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-09-20 22:14:03 +0900

    サーバー立ち上げ

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import * as http from 'http';
+// import * as http from 'http';
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{"content-Type":'text/plain; charset=utf-8'})
-    res.end('hello node.js')
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{"content-Type":'text/plain; charset=utf-8'})
+//     res.end('hello node.js')
+// })
+
+// const PORT=8888;
+// server.listen(PORT,()=>{
+//     console.log("サーバーが起動しました")
+// })
+
+import express from 'express'
+
+const app=express();
+const PORT =8888;
+
+app.get('/',(req,res)=>{
+    res.send("HELLO webserver");
 })
 
-const PORT=8888;
-server.listen(PORT,()=>{
-    console.log("サーバーが起動しました")
+app.listen(PORT,()=>{
+    console.log("サーバが起動しました")
 })
