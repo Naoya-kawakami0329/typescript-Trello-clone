f376adcad6a1039d74057e93960c359b46ae98b7
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-09-21 00:01:58 +0900

    API作成

--- index.ts
+++ index.ts
@@ -13,10 +13,29 @@
 import express from 'express'
 
 const app=express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 const PORT =8888;
 
 app.get('/',(req,res)=>{
-    res.send("HELLO webserver");
+   res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get("/users/:id",(req,res)=>{
+    res.send(`User id is ${req.params.id}.Name is ${req.query.name}`)
+})
+
+app.post('/',(req,res)=>{
+res.send(req.body);
+})
+
+app.put("/users/:id",(req,res)=>{
+    res.send(req.body)
+})
+
+app.delete("/users/:id",(req,res)=>{
+    res.send(req.params.id);
 })
 
 app.listen(PORT,()=>{
